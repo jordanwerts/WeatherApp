d2350d316f044dda798c69148025bd016fe76bee
Jordan Werts <jordan.werts@gmail.com>
2021-07-31 11:03:39 -0500

    removed app content and stated basic welcome page

---
+++ src/componnents/WelcomePage.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+export default function WelcomePage() {
+const [forecastDays, setForecastDays] = React.useState(3)
+
+const selections = [
+    {label: "3 Day", value: 3},
+    {label: "5 Day", value: 5},
+    {label: "7 Day", value: 7}
+]
+const forecastButtons = selections.map((item) => (
+    <button 
+        id = {item.value}
+        onClick = {() => setForecastDays(item.value)}
+    >
+        {item.label}
+    </button>
+))
+    const layout =(
+        <div>
+            <h2>Offical OKCoders Weather Forecast App</h2>
+            {forecastButtons}
+            <p>{forecastDays}</p>
+        </div>
+    )
+    return layout
+  }
+
